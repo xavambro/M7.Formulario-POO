f3f6e79468e8ed0a3939aaced945cf322b801868
xavambro <xavambro@gmail.com>
2021-02-11 16:44:28 +0100

    Nivel 2

--- js/form.js
+++ js/form.js
@@ -5,16 +5,32 @@ class Product{
         this.year = year;
         
     }
-    
-    
-    
+    showProduct(){
+        let newDiv = document.createElement("div");
+        let newButton = document.createElement("button")
+        newDiv.textContent = `Product Name: ${this.name} Product Price: ${this.price} Product Year: ${this.year}`
+        newButton.className="btn btn-warning";
+        newButton.textContent = "Delete";
+        newDiv.appendChild(newButton);
+        document.getElementById("products").appendChild(newDiv);
 
+    };
+
+    removeProduct(){
+
+    }
 }
+
 let form = document.forms[0];
 form.onsubmit = addProduct;
 
 function addProduct(e){
     e.preventDefault();
     let product = new Product(form.name.value,form.price.value,form.year.value);
+    product.showProduct();
     console.log(product);
+}
+
+function(){
+
 }
